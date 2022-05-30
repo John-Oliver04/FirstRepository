82049e3d83e5ad103ba713f3e423d4a479721b15
John-Oliver04 <106477188+John-Oliver04@users.noreply.github.com>
2022-05-30 10:19:29 +0800

    Add files via upload

---
+++ firstspringweb/firstspringweb/src/main/java/com/virola/firstspringweb/FirstspringwebApplication.java
@@ -0,0 +1,13 @@
+package com.virola.firstspringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstspringwebApplication.class, args);
+	}
+
+}
