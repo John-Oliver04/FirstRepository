513ec22afb61c96d690bca53ae130c7e96aec553
John-Oliver04 <106477188+John-Oliver04@users.noreply.github.com>
2022-05-30 10:33:52 +0800

    Update Student.java

--- firstspringweb/firstspringweb/src/main/java/com/virola/firstspringweb/Student.java
+++ firstspringweb/firstspringweb/src/main/java/com/virola/firstspringweb/Student.java
@@ -1,9 +1,26 @@
+package com.virola.firstspringweb;
+
 public class Student{
   private Long id;
   private String firstName;
   private String lastName;
   
+  public Student(){
+    
+  }
   
+  public Student(Long id, String firstName, String lastName){
+   this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    
+  }
   
+  private Long getId(Long id){
+   this.id = id; 
+  }
   
+  private setId(Long id){
+   this.id = id; 
+  }
 }
